7534663a3f963bd994266b3fd656f4cf22b737d9
rajesh.algorisys <rajesh.algorisys@ext.idfy.com>
2023-05-02 16:32:54 +0530

    03.1-arrays-method

--- 03-arrays.zig
+++ 03-arrays.zig
@@ -1,4 +1,6 @@
 // Arrays are denoted by [N]T, where N is the number of elements in the 
+
+
 // array and T is the type of those elements (i.e., the array’s child type).
 
 // For array literals, N may be replaced by _ to infer the size of the array
@@ -12,4 +14,16 @@ pub fn main() void {
     print("Length of array {s} is {}\n", .{array, array.len});
     print("Address of array {s} is  {*}\n", .{a, &a});
     print("Value at index 2 of array {s} is {c}\n", .{a, a[2]});
+
+    printArray(array);
+}
+
+
+fn printArray(array: anytype) void {
+  var arrayLen: u8 = array.len;
+  var i: u8 = 0;
+  while(i < arrayLen) : (i += 1) {
+    print("{c}", .{array[i]});
+  }
+  print("\n", .{});
 }
